c6d92228a130425228376c99669c32a93c87a9d8
acvanp <acvanp@gmail.com>
2020-09-14 19:55:28 -0400

    lively variations

--- CreativeGameofLife.R
+++ CreativeGameofLife.R
@@ -4,6 +4,10 @@
 
 # function that takes in an iteration of a game of life
 # outputs an iteration of a random game of life with different rules and patterns
+
+
+
+
 foo = function(i,d,pop){
   if(i==1){
     d <- (pop == 3) | (d & (pop>=bitwXor(pop, pop-d)) & (pop<=bitwOr(pop, pop+d)))#fire
@@ -36,11 +40,38 @@ foo = function(i,d,pop){
     d <- (pop %in% c(1,5)) | (d & (pop>=mean(bitwXor(pop, pop+d))) & (pop<=mean(bitwOr(pop, pop+d))))# vertical maze
   }
   if(i==11){
-    d<- (pop==3) | (d & (pop>=2) & (pop<=3))
+    d<- (pop==3) | (d & (pop>=2) & (pop<=3))#Conway's game of life
+  }
+  if(i==12){
+    d <- (pop == c(1,5)) | (d & (pop>=bitwAnd(pop+2, pop+d)) & (pop<=bitwOr(pop, pop-d)))#inchworms
+  }
+  
+  if(i==13){
+    d <- (pop == c(3,5)) | (d & (pop>=bitwAnd(pop+2, pop+d)) & (pop<=bitwOr(pop, pop-d)))#???
+  }
+  
+  if(i==14){
+    d <- (pop == c(1,3)) | (d & (pop>=bitwAnd(pop+2, pop+d)) & (pop<=bitwOr(pop, pop-d)))#inchworms
+  }
+  
+  
+  if(i==15){
+    d <- (pop == c(4,6)) | (d & (pop>=bitwAnd(pop+2, pop+d)) & (pop<=bitwOr(pop, pop-d)))#quick fade?
+  }
+  if(i==16){
+    d <- (pop == bitwXor(pop, pop-d)) | (d & (pop>=4) & (pop<=6))#new negative space
+  }
+  if(i==17){
+    d <- (pop == bitwXor(pop, pop-d)) | (d & (pop>=5) & (pop<=7))#period 6 negative space
+  }
+  if(i==18){
+    d <- (pop == bitwXor(pop, pop+d)) | (d & (pop>=5) & (pop<=7))#variable negative space
+  }
+  if(i==19){
+    d <- (pop == bitwXor(pop, pop+d)) | (d & (pop>=3) & (pop<=4))#blocky negative space
   }
   return(d)
 }
-
 
 
 # -------------------
@@ -49,10 +80,11 @@ library(reshape2)
 library(numbers)
 library(tidyr)
 setwd("C://Users//Lenovo//Demo//gameoflifeImages")
-ll=1e2
-ww =1e2
+ll=33
+ww =99
 tt = 1000
 d = matrix(sample(c(0,0,1), ll*ww, replace=TRUE), ncol = ww, nrow = ll)
+d = matrix(0, ncol = ww, nrow = ll)
 u = c()
 x = 6
 for(i in 1:tt){
@@ -70,9 +102,8 @@ for(i in 1:tt){
   d_rd <- cbind(0, d_ed[ , -ncol, drop = FALSE])
   pop <- d_eu + d_ed + d_le + d_re + d_lu + d_ru + d_ld + d_rd
   
-  
-  if(i > 14){if(abs(mean(u[(i-1):(i-3)])-mean(u[(i-10):(i-12)])) > 0.005*ll*ww){
-    d=foo(x,d,pop); cols = "blue" }else{x=sample(seq(1,11),1);d = foo(x,d,pop); cols= "red"}
+  if(i > 14){if(abs(mean(u[(i-1):(i-3)])-mean(u[(i-10):(i-12)])) > 0.002*ll*ww){
+    d=foo(x,d,pop); cols = "blue" }else{x=sample(seq(1,19),1);d = foo(x,d,pop); cols= "red"}
   }else{d=foo(x,d,pop); cols = "blue"}
   
   colnames(d) = seq(1,ncol(d))
@@ -86,19 +117,23 @@ for(i in 1:tt){
   
   if(i < 10){nnn = "000"} else if(i < 100){nnn = "00"} else if(i < 1000){nnn = "0"}else{nnn = ""}
   
+  aa = sample(ll,1)
+  bb=sample(ww,1)
+  #if(i%%20==0){d[aa,bb] = !d[aa,bb]}
+  
   d[d==TRUE] = 1
   d[d==FALSE] = 0
   
   u = c(u, sum(d))  
-  
+  #cols = rep(c("red", "magenta", "blue", "cyan", "green", "yellow", "orange"),tt/6)#rainbow colors
   png(paste0("gameoflife",nnn,i,".png"), width=650, heigh=800)
   layout(matrix(c(rep(1,12),rep(2,4)), 4, 4, byrow = TRUE))
   par(mar = c(0.1,0.1,0.1,0.1))
-  plot(md$key, md$variable2, pch = 15, cex = 1.4, ylim = c(0,nrow(d)), 
-       xlim = c(0,ncol(d)),
+  plot(md$key, md$variable2, pch = 15, cex = 1.5, 
+       ylim = c(0,ww), xlim = c(0,ll),
        axes = "false", xlab = NA, ylab = NA, col = cols)
-  plot(u, xlab = NA, ylab = NA, ylim = c(ll*ww*0.05, ll*ww*0.75), xlim = c(0,tt), type = "l")
-  mtext("Total Population", 3, -1, outer=FALSE, cex = 0.8)
+  plot(u, xlab = NA, ylab = NA, ylim = c(0, ll*ww), xlim = c(0,tt), type = "l")
+  mtext(paste("Total Population  rule",x), 3, -1, outer=FALSE, cex = 0.8)
   dev.off()
   
   
@@ -112,11 +147,15 @@ library(dplyr)
 library(purrr)
 
 p <- "gameoflife%04d.png"
-out <- "sequence72.gif"
+out <- "sequence96.gif"
 
-ffmpeg(pattern = p, output = out, fps.out = 20, delay = 0.05, overwrite = TRUE)
+ffmpeg(pattern = p, output = out, fps.out = 10, delay = 0.05, overwrite = TRUE)
 
 dev.off()
 file.remove(list.files(pattern=".png"))
 
 
+# make a kaleidascope effect by using odd dimensions 99x99 and starting with all zero matrix and
+# no random changing of grid cells
+# even dimensions gives a rorschach effect
+# even by odd dimensions gives 2-way assymmetry or rotational symmetry kind of
